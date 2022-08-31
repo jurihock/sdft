98fbfa31c08f9325a32b8e1e7f98838eb8ee1f6a
Juergen Hock <juergen.hock@jurihock.de>
2022-08-31 12:34:58 +0200

    Get rid of msvc warning 4996

--- test/dump.h
+++ test/dump.h
@@ -1,15 +1,16 @@
 #pragma once
 
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable:4996)
+#endif
+
 #if defined(__cplusplus)
 
   #include <cstdio>
 
   void dump(const char* path, const std::complex<double>* data, const size_t size)
   {
-    #if defined(_MSC_VER)
-    #pragma warning(suppress:4996)
-    #endif
-
     FILE* file = fopen(path, "wb");
     fwrite(data, sizeof(std::complex<double>), size, file);
     fclose(file);
@@ -21,13 +22,13 @@
 
   void dump(const char* path, const sdft_double_complex_t* data, const size_t size)
   {
-    #if defined(_MSC_VER)
-    #pragma warning(suppress:4996)
-    #endif
-
     FILE* file = fopen(path, "wb");
     fwrite(data, sizeof(sdft_double_complex_t), size, file);
     fclose(file);
   }
 
+#endif
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
 #endif
