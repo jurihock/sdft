3966ccbd7df0540467bdb33ccfca74437bdcb0d9
Jürgen Hock <juergen.hock@jurihock.de>
2022-12-07 08:31:51 +0100

    Cleanup #4

--- test/stft.py
+++ test/stft.py
@@ -8,7 +8,6 @@ class STFT:
     def __init__(self, framesize, hopsize, window='hann', shift=False):
 
         self.framesize = framesize
-        self.dftsize = numpy.fft.rfftfreq(framesize).size
         self.hopsize = hopsize
         self.window = window
         self.shift = shift
@@ -19,9 +18,9 @@ class STFT:
 
         assert samples.ndim == 1, f'Expected 1D array (samples,), got {samples.shape}!'
 
-        frames = sliding_window_view(samples, self.framesize, writeable=False)[::self.hopsize]
+        W = self.weights()
 
-        M, N, W = frames.shape[0], self.dftsize, self.weights()
+        frames = sliding_window_view(samples, self.framesize, writeable=False)[::self.hopsize]
 
         dfts = self.fft(frames * W)
 
@@ -33,11 +32,11 @@ class STFT:
 
         assert dfts.ndim == 2, f'Expected 2D array (samples,frequencies), got {dfts.shape}!'
 
-        M, W = dfts.shape[0] * self.hopsize + self.framesize, self.weights()
+        N, W = dfts.shape[0] * self.hopsize + self.framesize, self.weights()
 
-        W *= self.hopsize / numpy.sum(W**2)
+        W *= self.hopsize / numpy.sum(W**2)  # unity gain
 
-        samples = numpy.zeros((M), float)
+        samples = numpy.zeros((N), float)
 
         frames = sliding_window_view(samples, self.framesize, writeable=True)[::self.hopsize]
         frames += self.ifft(dft) * W
@@ -64,20 +63,19 @@ class STFT:
 
     def weights(self):
 
-        size = self.framesize
-        window = str(self.window).lower()
+        N, W = self.framesize, str(self.window).lower()
 
-        if window in 'hann':
+        if W in 'hann':
 
-            return 0.5 - 0.5 * numpy.cos(2 * numpy.pi * numpy.arange(size) / size)
+            return 0.5 - 0.5 * numpy.cos(2 * numpy.pi * numpy.arange(N) / N)
 
-        if window in 'hamming':
+        if W in 'hamming':
 
-            return 0.54 - 0.46 * numpy.cos(2 * numpy.pi * numpy.arange(size) / size)
+            return 0.54 - 0.46 * numpy.cos(2 * numpy.pi * numpy.arange(N) / N)
 
-        if window in 'blackman':
+        if W in 'blackman':
 
-            return 0.42 - 0.5  * numpy.cos(2 * numpy.pi * numpy.arange(size) / size) \
-                        + 0.08 * numpy.cos(4 * numpy.pi * numpy.arange(size) / size)
+            return 0.42 - 0.5  * numpy.cos(2 * numpy.pi * numpy.arange(N) / N) \
+                        + 0.08 * numpy.cos(4 * numpy.pi * numpy.arange(N) / N)
 
-        return numpy.ones(size)
+        return numpy.ones(N)
